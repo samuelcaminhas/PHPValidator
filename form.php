4095b370b7442d82beb533fa358402cf309e1290
Samuel Caminhas <samuel.caminhas@aluno.ifsp.edu.br>
2023-09-12 19:44:45 -0300

    Update form.php
    Select correct status

--- form.php
+++ form.php
@@ -23,35 +23,10 @@
     </form>
     <p>Estado:</p>
     <form action="teste.php" method="post">
-        <label for="estado">Selecione um estado:</label>
+        <label for="estado">Selecione um estado civil:</label>
         <select id="estado" name="estado">
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
+            <option value="S">Solteiro</option>
+            <option value="M">Casado</option>
         </select>
         <input type="submit" value="Enviar">
     </form>
