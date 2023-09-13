c222a45050021a0f7bcfb7dea4c3e0b1efa1e10b
samuelcaminhas <samuel.caminhas@aluno.ifsp.edu.br>
2023-09-13 20:56:21 -0300

    last commit
    add css :+1:
    add all validations :+1:
    
    finished

--- form.php
+++ form.php
@@ -1,57 +1,118 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
 <head>
-    <meta charset="utf-8" />
-    <title>POST</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulários</title>
+  <style>
+      body {
+          font-family: Arial, sans-serif;
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 20px;
+          display: flex;
+          justify-content: space-between;
+      }
+
+      .column-left {
+          flex: 1;
+          margin-right: 20px; /* Aumentei para 20px */
+      }
+
+      .column-right {
+          flex: 1;
+          margin-left: 20px; /* Aumentei para 20px */
+      }
+
+      p {
+          margin-bottom: 5px;
+      }
+
+      input, select {
+          width: 100%;
+          padding: 8px;
+          margin-bottom: 10px;
+          box-sizing: border-box;
+      }
+
+      label {
+          margin-bottom: 5px;
+          display: block;
+      }
+
+      input[type="submit"] {
+          background-color: #4CAF50;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          cursor: pointer;
+      }
+
+      input[type="submit"]:hover {
+          background-color: #45a049;
+      }
+  </style>
 </head>
 
 <body>
+<div class="column-left">
+  <p>Nome:</p>
+  <form action="teste.php" method="post">
+    <input name="nome" type="text" placeholder="Seu nome..." />
+    <input name="enviarNome" type="submit" value="Enviar" />
+  </form>
 
-    <p>Nome:</p>
-    <form action="teste.php" method="post">
-        <input name="nome" type="text" placeholder="Seu nome..." />
-        <input name="enviar" type="submit" value="Enviar" />
-    </form>
-    <p>Idade:</p>
-    <form action="teste.php" method="post">
-        <input name="idade" type="number" placeholder="Sua idade" />
-        <input name="enviar" type="submit" value="Enviar" />
-    </form>
-    <p>Email:</p>
-    <form action="teste.php" method="post">
-        <input name="email" type="email" placeholder="seu@email.com" />
-        <input name="enviar" type="submit" value="Enviar" />
-    </form>
-    <p>Estado:</p>
-    <form action="teste.php" method="post">
-        <label for="estado">Selecione um estado civil:</label>
-        <select id="estado" name="estado">
-            <option value="S">Solteiro</option>
-            <option value="M">Casado</option>
-        </select>
-        <input type="submit" value="Enviar">
-    </form>
+  <p>Email:</p>
+  <form action="teste.php" method="post">
+    <input name="email" type="email" placeholder="seu@email.com" />
+    <input name="enviarEmail" type="submit" value="Enviar" />
+  </form>
 
-    <p>O que você gostaria de comer hoje?</p>
-    <form action="teste.php" method="post">
-        <input type="checkbox" id="comida1" name="peito" value="peito">
-        <label for="comida1"> Peito de frango</label><br>
-        <input type="checkbox" id="comida2" name="bife" value="bife">
-        <label for="comida2"> Bife de Alcatra</label><br>
-        <input type="checkbox" id="comida3" name="pure" value="pure">
-        <label for="comida3"> Pure de batata</label>
-    </form>
+  <p>Idade:</p>
+  <form action="teste.php" method="post">
+    <input name="idade" type="number" placeholder="Sua idade" />
+    <input name="enviarIdade" type="submit" value="Enviar" />
+  </form>
 
-    <p>Defina a forma de entrega</p>
+  <p>Estado:</p>
+  <form action="teste.php" method="post">
+    <label for="estado">Selecione um estado civil:</label>
+    <select id="estado" name="estado">
+      <option value="S">Solteiro</option>
+      <option value="M">Casado</option>
+    </select>
+    <input name="enviarEstado"type="submit" value="Enviar">
+  </form>
+</div>
 
-      <form>
-      <input type="radio" id="Entrega" name="met_entrega" value="Entrega">
-      <label for="Entrega">Entrega</label><br>
-      <input type="radio" id="Retirada" name="met_entrega" value="Retirada">
-      <label for="Retirada">Retirada</label><br>
-      </form>
+<div class="column-right">
+  <p>O que você gostaria de comer hoje?</p>
+  <form action="teste.php" method="post">
+    <input type="checkbox" id="comida1" name="peito" value="peito">
+    <label for="comida1"> Peito de frango</label><br>
+    <input type="checkbox" id="comida2" name="bife" value="bife">
+    <label for="comida2"> Bife de Alcatra</label><br>
+    <input type="checkbox" id="comida3" name="pure" value="pure">
+    <label for="comida3"> Pure de batata</label>
+    <input type="checkbox" id="comida4" name="arroz" value="arroz">
+    <label for="comida4"> Bife de Alcatra</label><br>
+    <input type="checkbox" id="comida5" name="feijao" value="feijao">
+    <label for="comida5"> Pure de batata</label>
+    <input name="enviarComida" type="submit" value="Enviar">
+  </form>
 
-    <div id="mensagem_erro" style="color: red;"></div>
+  <p>Defina a forma de entrega</p>
+  <form action="teste.php" method="post">
+    <input type="radio" id="Entrega" name="met_entrega" value="Entrega">
+    <label for="Entrega">Entrega</label><br>
+    <input type="radio" id="Retirada" name="met_entrega" value="Retirada">
+    <label for="Retirada">Retirada</label><br>
+    <input type="submit" value="Enviar">
+  </form>
+</div>
+
 
 </body>
-</html>
 
+</html>
--- teste.php
+++ teste.php
@@ -4,22 +4,91 @@ $nome = $_POST["nome"];
 $idade = $_POST["idade"];
 $email = $_POST["email"];
 $estado = $_POST["estado"];
-$peito = $_POST["peito"];
-$bife = $_POST["bife"];
-$pure = $_POST["pure"];
-$met_entrega = $_POST["met_entrega"];
-$erro  = 0;
+$metodoEntrega = $_POST['met_entrega'];
+$opcoesSelecionadas = 0;
 
 
 
-if(isset($_POST['enviar'])) {
+if(isset($_POST['enviarNome'])) {
     if(empty($nome)) {
         echo "<script>alert('O campo de nome não pode estar vazio.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
     } elseif (strlen($nome) < 4 || strlen($nome) > 10) {
         echo "<script>alert('O nome deve ter de 4 a 10 caracteres.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
     } else {
-        echo "Ok!";
+        echo "<script>alert('Ok!.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
     }
 }
 
+if(isset($_POST['enviarIdade'])) {
+    if(empty($idade)) {
+        echo "<script>alert('O campo de idade não pode estar vazio.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    } elseif (($idade) < 18 || ($idade) > 60) {
+        echo "<script>alert('A idade não pode ser menor que 18 ou maior que 60');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    } else {
+        echo "<script>alert('Ok!.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    }
+}
+
+
+if(isset($_POST['enviarEmail'])) {
+    if(empty($email)) {
+        echo "<script>alert('O campo de e-mail não pode estar vazio.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<script>alert('O e-mail inserido não é válido.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    } else {
+        echo "<script>alert('Ok!.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    }
+}
+
+
+
+if(isset($_POST['enviarEstado'])){
+    if(empty($estado)) {
+        echo "<script>alert('Por favor, selecione um estado.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    } else{
+        echo "<script>alert('Ok!.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    }
+}
+
+if(isset($_POST['enviarComida'])) {
+    $opcoes = ['peito', 'bife', 'pure', 'arroz', 'feijao'];
+    foreach($opcoes as $opcao) {
+        if(isset($_POST[$opcao])) {
+            $opcoesSelecionadas++;
+        }
+    }
+    if($opcoesSelecionadas >= 3) {
+        echo "<script>alert('Ok!.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    } else {
+        echo "<script>alert('Selecione ao menos 3 opçoes.');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    }
+}
+
+
+if (isset($_POST['met_entrega'])) {
+    if ($metodoEntrega == 'Entrega' || $metodoEntrega == 'Retirada') {
+        echo "<script>alert('Método de entrega válido: ".$metodoEntrega."');</script>";
+        echo "<script>window.location = 'form.php';</script>";
+    }
+} else {
+    echo "<script>alert('Favor selecionar um metodo de entrega');</script>";
+    echo "<script>window.location = 'form.php';</script>";
+}
+
+
 ?>
+
+
